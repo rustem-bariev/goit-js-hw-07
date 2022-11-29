868bcf97f320fbad5b97341cd808f035127dc397
rustem-bariev <rustem.bariev.2012@gmail.com>
2022-11-30 00:25:13 +0200

    create image-galary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryPlace = document.querySelector(`.gallery`);
 
+const changeGalleryOfImage = galleryItems.map(galleryItem => {
+    const changeItemEl = document.createElement(`a`);
+    changeItemEl.href = galleryItem.original;
+    changeItemEl.classList.add(`gallery__link`);
+    const changeImgEl = document.createElement(`img`);
+    changeImgEl.classList.add(`gallery__image`);
+    changeImgEl.src = galleryItem.preview;
+    changeImgEl.alt = galleryItem.description;
+    changeImgEl.dataset.source = galleryItem.original;
+
+    changeItemEl.appendChild(changeImgEl);
+
+    return changeItemEl;
+});
+galleryPlace.append(...changeGalleryOfImage);
+galleryPlace.addEventListener(`click`, callModalWindow);
+
+function callModalWindow(event) {
+  event.preventDefault();
+  if (event.target.nodeName != 'IMG') {
+    return;
+  }
+  // console.log(event.target.dataset.source);
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="${event.target.alt}" width="800">`
+  );
+  instance.show();
+
+  document.addEventListener('keydown', event => {
+    const faidModal = document.querySelector(`div.basicLightbox`);
+    if (event.key === 'Escape' && faidModal !== null) {
+      instance.close();
+    }
+  });
+}
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+const galleryPlace = document.querySelector(`.gallery`);
 console.log(galleryItems);
+const changeGalleryOfImage = galleryItems.map(galleryItem => {
+    const changeItemEl = document.createElement(`a`);
+    changeItemEl.href = galleryItem.original;
+    changeItemEl.classList.add(`gallery__link`);
+
+    const changeImgEl = document.createElement(`img`);
+    changeImgEl.classList.add(`gallery__image`);
+    changeImgEl.src = galleryItem.preview;
+    changeImgEl.alt = galleryItem.description;
+    changeImgEl.dataset.source = galleryItem.original;
+
+    changeItemEl.appendChild(changeImgEl);
+
+    return changeItemEl;
+});
+galleryPlace.append(...changeGalleryOfImage);
+galleryPlace.addEventListener(`click`, callModalWindow);
+
+function callModalWindow(event) {
+  event.preventDefault();
+  if (event.target.nodeName != 'IMG') {
+    return;
+  }
+
+  var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: `alt`,
+    captionDelay: 250,
+  });
+}
+
