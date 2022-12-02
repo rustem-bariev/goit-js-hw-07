97c816c874e0418208cbe09e4eb95611d2663cc5
rustem-bariev <rustem.bariev.2012@gmail.com>
2022-12-02 18:39:00 +0200

    fix

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,32 +3,29 @@ import { galleryItems } from './gallery-items.js';
 const galleryPlace = document.querySelector(`.gallery`);
 console.log(galleryItems);
 const changeGalleryOfImage = galleryItems.map(galleryItem => {
-    const changeItemEl = document.createElement(`a`);
-    changeItemEl.href = galleryItem.original;
-    changeItemEl.classList.add(`gallery__link`);
+const changeItemEl = document.createElement(`a`);
+changeItemEl.href = galleryItem.original;
+changeItemEl.classList.add(`gallery__link`);
 
-    const changeImgEl = document.createElement(`img`);
-    changeImgEl.classList.add(`gallery__image`);
-    changeImgEl.src = galleryItem.preview;
-    changeImgEl.alt = galleryItem.description;
-    changeImgEl.dataset.source = galleryItem.original;
+const changeImgEl = document.createElement(`img`);
+changeImgEl.classList.add(`gallery__image`);
+changeImgEl.src = galleryItem.preview;
+changeImgEl.alt = galleryItem.description;
+changeImgEl.dataset.source = galleryItem.original;
 
-    changeItemEl.appendChild(changeImgEl);
+changeItemEl.appendChild(changeImgEl);
 
-    return changeItemEl;
+return changeItemEl;
 });
 galleryPlace.append(...changeGalleryOfImage);
 galleryPlace.addEventListener(`click`, callModalWindow);
 
 function callModalWindow(event) {
   event.preventDefault();
-  if (event.target.nodeName != 'IMG') {
-    return;
   }
 
-  var lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: `alt`,
-    captionDelay: 250,
-  });
-}
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
 
